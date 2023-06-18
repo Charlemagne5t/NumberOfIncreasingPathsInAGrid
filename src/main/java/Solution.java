5bb014e8e8b33a08fcea2c5a1b4f2db9d72bdb02
alek <wait82985@gmail.com>
2023-06-18 16:26:18 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,44 @@
 public class Solution {
     public int countPaths(int[][] grid) {
-        //TODO
-        return 0;
+        int modulo = 1_000_000_000 + 7;
+        int m = grid.length;
+        int n = grid[0].length;
+        Integer[][] memo = new Integer[m][n];
+        int answer = 0;
+        int[][] numberOfPathsFromEachCell = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                numberOfPathsFromEachCell[i][j] = dfs(grid, i, j, memo) % modulo;
+                answer = (answer + numberOfPathsFromEachCell[i][j]) % modulo;
+            }
+        }
+
+        return answer;
+    }
+
+    private int dfs(int[][] grid, int i, int j, Integer[][] memo) {
+        int modulo = 1_000_000_000 + 7;
+        if (memo[i][j] != null) {
+            return memo[i][j];
+        }
+        int result = 0;
+        //NORTH
+        if (i > 0 && grid[i][j] < grid[i - 1][j]) {
+            result = (result + dfs(grid, i - 1, j, memo)) % modulo;
+        }
+        //EAST
+        if (j < grid[i].length - 1 && grid[i][j] < grid[i][j + 1]) {
+            result = (result + dfs(grid, i, j + 1, memo)) % modulo;
+        }
+        //SOUTH
+        if (i < grid.length - 1 && grid[i][j] < grid[i + 1][j]) {
+            result = (result + dfs(grid, i + 1, j, memo)) % modulo;
+        }
+        //WEST
+        if (j > 0 && grid[i][j] < grid[i][j - 1]) {
+            result = (result + dfs(grid, i, j - 1, memo)) % modulo;
+        }
+        memo[i][j] = result + 1;
+        return result + 1;
     }
 }
